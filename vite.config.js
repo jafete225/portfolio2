5e3cdee64242a395c5bfd737d3240df8c844ff08
jafete225 <73774270+jafete225@users.noreply.github.com>
2024-10-20 10:52:29 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Remova a opção external se não for necessário
+    rollupOptions: {
+      // External pode ser utilizado se você tem bibliotecas externas que não quer incluir
+      // Se não houver necessidade, pode ser removido
+      // external: [], 
+    },
+  },
+});
